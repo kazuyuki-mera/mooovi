0ef0234cfb7b8db0efa6597983712224ac0d116d
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-05-04 23:25:41 +0900

    投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TechReviewSite::Application.routes.draw do
   root 'products#index'
   resources :products, only: :show do
+    resources :reviews, only: [:new, :create]
     collection do
       get 'search'
     end
