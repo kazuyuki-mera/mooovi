d1340e47f2d251be25356f7df81b9885e207b147
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-05-05 14:50:43 +0900

    サインアウト、ログイン機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < RankingController
+  # ログインしていない状態でレビューの投稿をしようとすると、ログイン画面にリダイレクト
+  before_action :authenticate_user!, only: :search
   def index
     # productsテーブルから最新順に作品を20件取得する
     @products = Product.order('id ASC').limit(20)
