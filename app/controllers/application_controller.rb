5449a812545cd53996cc2c39ee1cfa81a3cc91d0
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-05-05 15:18:00 +0900

    ユーザー登録機能の充実

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # deviseコントローラのアクションが動いた時のみ対応
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # サインアウト後にサインイン画面に遷移
   def after_sign_out_path_for(resource)
-    # サインアウト後にサインイン画面に遷移
     new_user_session_path
   end
+
+  # サインアップ時にnicknameとavatarを許可する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_055333) do
+ActiveRecord::Schema.define(version: 2020_05_05_055833) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_05_05_055333) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "nickname"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
