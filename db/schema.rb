8749b9bde9f00832ecdf8abb52f26852f94cc963
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-05-05 20:00:01 +0900

    アソシエーションを利用

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_055833) do
+ActiveRecord::Schema.define(version: 2020_05_05_103413) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 2020_05_05_055833) do
   end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname"
     t.integer "rate"
     t.text "review"
     t.integer "product_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
