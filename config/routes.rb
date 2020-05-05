e2cf80140bb3ef95a81f2489909418610255b332
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-05-05 20:27:32 +0900

    マイページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TechReviewSite::Application.routes.draw do
   devise_for :users
   root 'products#index'
+  resources :users, only: :show
   resources :products, only: :show do
     resources :reviews, only: [:new, :create]
     collection do
